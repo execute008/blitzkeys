1ccf8707f50babcf6b721822c17e4d5de8af0c76
Oskar Freye <oskar@freye.tech>
2025-11-09 21:55:01 +0100

    feat(branding): update header with theme-aware logos and project links
    - Replace Phoenix logo with dual logo-light.png and logo-dark.png images
    - Update navigation links from Phoenix docs to freye.tech and GitHub repo
    - Remove "Get Started" button from header
    - Update page title from "Phoenix Framework" suffix to just "Blitzkeys"
    - Add target="_blank" and rel="noopener noreferrer" to external links
    
    This completes the rebrand from Phoenix defaults to Blitzkeys identity

--- lib/blitzkeys_web/components/layouts.ex
+++ lib/blitzkeys_web/components/layouts.ex
@@ -47,25 +47,44 @@ defmodule BlitzkeysWeb.Layouts do
     <header class="navbar px-4 sm:px-6 lg:px-8">
       <div class="flex-1">
         <a href="/" class="flex-1 flex w-fit items-center gap-2">
-          <img src={~p"/images/logo.svg"} width="36" />
-          <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
+          <img
+            src={~p"/images/logo-light.png"}
+            width="120"
+            class="logo-light"
+            alt="Blitzkeys"
+          />
+          <img
+            src={~p"/images/logo-dark.png"}
+            width="120"
+            class="logo-dark"
+            alt="Blitzkeys"
+          />
         </a>
       </div>
       <div class="flex-none">
         <ul class="flex flex-column px-1 space-x-4 items-center">
           <li>
-            <a href="https://phoenixframework.org/" class="btn btn-ghost">Website</a>
+            <a
+              href="https://freye.tech"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="btn btn-ghost"
+            >
+              Website
+            </a>
           </li>
           <li>
-            <a href="https://github.com/phoenixframework/phoenix" class="btn btn-ghost">GitHub</a>
+            <a
+              href="https://github.com/execute008/blitzkeys"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="btn btn-ghost"
+            >
+              GitHub
+            </a>
           </li>
           <li>
             <.theme_toggle />
-          </li>
-          <li>
-            <a href="https://hexdocs.pm/phoenix/overview.html" class="btn btn-primary">
-              Get Started <span aria-hidden="true">&rarr;</span>
-            </a>
           </li>
         </ul>
       </div>
